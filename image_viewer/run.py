ea59ffe7e0061eed4093eab2796fd5b170d1e1a9
CPWAN <cpwan@ust.hk>
2019-11-08 22:54:06 +0800

    added image viewer

--- image_viewer/run.py
+++ image_viewer/run.py
@@ -19,6 +19,23 @@ def render():
     
     return render_template('./imshow.html', path_to_img=filename.split('.')[0])
 
+@app.route('/<ids>', methods=['GET','POST'])
+def render_by(ids):
+    global i
+    global imagepath
+    if request.method == 'POST':
+        if request.form['submit_btn'] == 'previous':
+            i-=1
+        elif request.form['submit_btn'] == 'next':
+            i+=1
+    i%=len(imagepath)
+    filename= imagepath[i].split('/')[-1]
+    
+    return render_template('./imshow.html', path_to_img=ids)
+
+
+
+
 if __name__ == '__main__':
 
     app.run(debug = True )
